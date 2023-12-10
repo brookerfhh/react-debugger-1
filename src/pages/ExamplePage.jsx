8a6924de0176f5480eb74760598585721153e11a
KingComedy <1455445725@qq.com>
2023-12-10 16:46:16 +0800

    note

--- src/pages/ExamplePage.jsx
+++ src/pages/ExamplePage.jsx
@@ -1,46 +1,66 @@
-import { useEffect } from "react";
-import {React, Component, useState} from "./CONST";
-
+import { useEffect } from 'react'
+import { React, Component, useState } from './CONST'
+const testhook = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [state, set] = useState()
+}
 class ClassComponent extends Component {
   state = {
-    count: 0
+    count: 0,
   }
   componentWillUnmount() {
     console.info('componentWillUnmount==')
   }
   render() {
     const { count } = this.state
-    return <div className="class border" onClick={() => this.setState({
-      count: count+1
-    })}>{count}</div>;
+    return (
+      <div
+        className="class border"
+        onClick={() =>
+          this.setState({
+            count: count + 1,
+          })
+        }
+      >
+        {count}
+      </div>
+    )
   }
 }
 
-function FunctionComponent({name}) {
+function FunctionComponent({ name }) {
   const [count, setCount] = useState(0)
   return (
     <div className="function border">
       {name}
       {count}
-      <button onClick={() => setCount(count+1)}>click</button>
+      <button onClick={() => setCount(count + 1)}>click</button>
     </div>
-  );
+  )
 }
 const App = () => {
   const [show, setShow] = useState(true)
   useEffect(() => {
-   return () => {
-    debugger
-   } 
+    return () => {
+      debugger
+    }
   })
   return (
     <div className="box border">
-      <p style={{border:'1px solid blue'}}>
-        <span>方<span>hh</span></span>
-        <button onClick={() => {setShow(false)}}>哈哈</button>
+      <p style={{ border: '1px solid blue' }}>
+        <span>
+          方<span>hh</span>
+        </span>
+        <button
+          onClick={() => {
+            setShow(false)
+          }}
+        >
+          哈哈
+        </button>
       </p>
       <ClassComponent name="class-hh"></ClassComponent>
-      {show && <FunctionComponent  name="function-hh"></FunctionComponent>}
+      {show && <FunctionComponent name="function-hh"></FunctionComponent>}
     </div>
   )
 }
@@ -50,9 +70,9 @@ const jsx = (
       <span>方</span>
       <span>哈哈</span>
     </p>
-    <FunctionComponent  name="哇哈哈"></FunctionComponent>
+    <FunctionComponent name="哇哈哈"></FunctionComponent>
     <ClassComponent name="哇哈哈"></ClassComponent>
   </div>
-);
+)
 console.log('react 版本===', React.version)
-export default App;
+export default App
